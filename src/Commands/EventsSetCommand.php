fc19cdc8840796b2e68ea72b5b46a9cc1edeb11f
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-01-26 13:03:48 +1300

    WIP

--- src/Commands/EventsSetCommand.php
+++ src/Commands/EventsSetCommand.php
@@ -13,6 +13,8 @@ class EventsSetCommand extends GeneratorCommand
 
     protected $description = 'Set of all [goldfinch/component-events] commands';
 
+    protected $no_arguments = true;
+
     protected function execute($input, $output): int
     {
         $command = $this->getApplication()->find(
--- src/Commands/EventsTemplatesCommand.php
+++ src/Commands/EventsTemplatesCommand.php
@@ -12,6 +12,8 @@ class EventsTemplatesCommand extends GeneratorCommand
 
     protected $description = 'Publish [goldfinch/component-events] templates';
 
+    protected $no_arguments = true;
+
     protected function execute($input, $output): int
     {
         $templater = Templater::create($input, $output, $this, 'goldfinch/component-events');
