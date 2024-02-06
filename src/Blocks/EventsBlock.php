e405d4f1b91ca5b4d920196112a5133dbc75e110
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-02-07 08:27:35 +1300

    WIP

--- src/Blocks/EventsBlock.php
+++ src/Blocks/EventsBlock.php
@@ -2,17 +2,12 @@
 
 namespace Goldfinch\Component\Events\Blocks;
 
-use Goldfinch\Fielder\Fielder;
-use Goldfinch\Mill\Traits\Millable;
-use Goldfinch\Fielder\Traits\FielderTrait;
-use DNADesign\Elemental\Models\BaseElement;
+use Goldfinch\Blocks\Models\BlockElement;
 use Goldfinch\Component\Events\Models\Nest\EventItem;
 use Goldfinch\Component\Events\Models\Nest\EventCategory;
 
-class EventsBlock extends BaseElement
+class EventsBlock extends BlockElement
 {
-    use FielderTrait, Millable;
-
     private static $table_name = 'EventsBlock';
     private static $singular_name = 'Events';
     private static $plural_name = 'Events';
@@ -23,11 +18,6 @@ class EventsBlock extends BaseElement
     private static $description = '';
     private static $icon = 'font-icon-p-event-alt';
 
-    public function fielder(Fielder $fielder): void
-    {
-        // ..
-    }
-
     public function Items()
     {
         return EventItem::get();
@@ -36,17 +26,5 @@ class EventsBlock extends BaseElement
     public function Categories()
     {
         return EventCategory::get();
-    }
-
-    public function getSummary()
-    {
-        return $this->getDescription();
-    }
-
-    public function getType()
-    {
-        $default = $this->i18n_singular_name() ?: 'Block';
-
-        return _t(__CLASS__ . '.BlockType', $default);
     }
 }
