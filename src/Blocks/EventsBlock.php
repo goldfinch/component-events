f6babc7bc8646821dd1221fd95049704168a2003
Art Karasev <goldfinch@users.noreply.github.com>
2024-02-10 15:26:03 +1300

    WIP

--- src/Blocks/EventsBlock.php
+++ src/Blocks/EventsBlock.php
@@ -3,11 +3,14 @@
 namespace Goldfinch\Component\Events\Blocks;
 
 use DNADesign\Elemental\Models\BaseElement;
+use Goldfinch\Helpers\Traits\BaseElementTrait;
 use Goldfinch\Component\Events\Models\Nest\EventItem;
 use Goldfinch\Component\Events\Models\Nest\EventCategory;
 
 class EventsBlock extends BaseElement
 {
+    use BaseElementTrait;
+
     private static $table_name = 'EventsBlock';
     private static $singular_name = 'Events';
     private static $plural_name = 'Events';
